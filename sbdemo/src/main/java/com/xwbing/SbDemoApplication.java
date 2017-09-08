d5bdfd6d5dbf1202e5e37ce54785e172350a113a
xiangwb <xiangwb@drore.com>
2017-09-08 17:05:44 +0800

    merge

--- sbdemo/src/main/java/com/xwbing/SbDemoApplication.java
+++ sbdemo/src/main/java/com/xwbing/SbDemoApplication.java
@@ -3,7 +3,7 @@ package com.xwbing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
---
+++ sbdemo/src/main/java/com/xwbing/configuration/ApplicationContextConfig.java
@@ -0,0 +1,31 @@
+package com.xwbing.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * 说明: 程序上下文配置
+ * 项目名称: sbdemo
+ * 创建时间: 2017/5/10 16:36
+ * 作者:  xiangwb
+ */
+@Configuration
+//@Import(xxx.class)//用来导入其他配置类
+//@ImportResource("classpath:applicationContext.xml")//用来加载xml配置文件
+public class ApplicationContextConfig {
+    /**
+     * 线程池
+     *
+     * @return
+     */
+    @Bean(name = "taskExecutor")
+    public static ThreadPoolTaskExecutor getPoolTaskExecutor() {
+        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+        poolTaskExecutor.setCorePoolSize(5);
+        poolTaskExecutor.setKeepAliveSeconds(30000);
+        poolTaskExecutor.setMaxPoolSize(1000);
+        poolTaskExecutor.setQueueCapacity(200);
+        return poolTaskExecutor;
+    }
+}
